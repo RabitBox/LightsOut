f4636eb1cf39947f58d7052431e51ea1da13030e
Gai Takakura <g.takakura@outlook.com>
2018-07-20 19:06:18 +0900

    ボタンの切り替え実装完了 5x5のパネルでの実装に移行

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -6,9 +6,22 @@ public class Button : MonoBehaviour
 {
 	private bool _isLight = true;	// ボタンが光っているか
 	public bool IsLight { get { return this._isLight; } }
+	MeshRenderer _renderer;
+
+	// Use this for initialization
+	private void Start()
+	{
+		_renderer = this.gameObject.GetComponent<MeshRenderer>();
+	}
 
 	public void SwitchLight()
 	{
-		_isLight = (_isLight == true) ? false : true;
+		_isLight = (_isLight == true) ? 
+			false :
+			true;
+
+		_renderer.material = (_isLight == true) ?
+			Resources.Load<Material>("Materials/On") :
+			Resources.Load<Material>("Materials/Off");
 	}
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class GameController : SingletonMonoBehavior<GameController>
 {
 	private List<Button> _buttons = new List<Button>();
-	[SerializeField]
-	private static Material[] _button_material = new Material[2];
 
 	RaycastHit _raycast_hit = new RaycastHit();
 
@@ -29,10 +27,28 @@ public class GameController : SingletonMonoBehavior<GameController>
 		{
 			Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			Physics.Raycast(_ray, out _raycast_hit);
-			if (_raycast_hit.collider.tag == "Button")
+			if(_raycast_hit.collider != null)
 			{
-				_raycast_hit.collider.gameObject.GetComponent<Button>().SwitchLight();
+				if (_raycast_hit.collider.tag == "Button")
+				{
+					_raycast_hit.collider.gameObject.GetComponent<Button>().SwitchLight();
+				}
 			}
 		}
+	}
+
+	// ボタンの状態をチェックする
+	// return[0] : 光っているボタンがある
+	// return[1] : ボタンが全て消えている
+	private bool CheckButtons()
+	{
+		foreach(var _button in _buttons)
+		{
+			if(_button.IsLight)
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 }
