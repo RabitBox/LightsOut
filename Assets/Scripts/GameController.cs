1e9ce8fac8323cc9d7229d4c1870e07692a7c5da
Gai Takakura <g.takakura@outlook.com>
2018-07-20 19:51:07 +0900

    自動生成完了

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameController : SingletonMonoBehavior<GameController>
 {
-	private List<Button> _buttons = new List<Button>();
+	private Button[,] _buttons;
+	[SerializeField]
+	private GameObject _button_prefab = null;
+	private readonly int _number = 5;
+	private readonly float _button_distance = 1.2f;
 
 	RaycastHit _raycast_hit = new RaycastHit();
 
 	// Use this for initialization
 	void Start()
 	{
-
+		_buttons = new Button[_number, _number];
+		CreateButtons();
 	}
 
 	// Update is called once per frame
@@ -42,13 +48,38 @@ public class GameController : SingletonMonoBehavior<GameController>
 	// return[1] : ボタンが全て消えている
 	private bool CheckButtons()
 	{
-		foreach(var _button in _buttons)
+		var _sequence =	from row	in Enumerable.Range(0, this._buttons.GetLength(0))
+						from column in Enumerable.Range(0, this._buttons.GetLength(1))
+						select new { row, column };
+
+		foreach(var _index in _sequence)
 		{
-			if(_button.IsLight)
+			if(this._buttons[_index.row, _index.column].IsLight)
 			{
 				return false;
 			}
 		}
 		return true;
+	}
+
+	// ボタンを生成する
+	private void CreateButtons()
+	{
+		if(this._buttons != null)
+		{
+			if(_button_prefab != null)
+			{
+				var _sequence = from row	in Enumerable.Range(0, this._buttons.GetLength(0))
+								from column in Enumerable.Range(0, this._buttons.GetLength(1))
+								select new { row, column };
+
+				foreach (var _index in _sequence)
+				{
+					var _created_object = Instantiate(_button_prefab, this.transform);
+					_created_object.transform.position = new Vector3(_index.column, _index.row) * _button_distance;
+					this._buttons[_index.row, _index.column] = _created_object.GetComponent<Button>();
+				}
+			}
+		}
 	}
 }
