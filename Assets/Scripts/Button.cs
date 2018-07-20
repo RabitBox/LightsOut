68cbc7fe73c6046e396cc4e02280c56f3a156646
Gai Takakura <g.takakura@outlook.com>
2018-07-20 16:00:35 +0900

    First Push

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+	private bool _isLight = true;	// ボタンが光っているか
+	public bool IsLight { get { return this._isLight; } }
+
+	// Use this for initialization
+	//void Start(){}
+
+	// Update is called once per frame
+	//void Update(){}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	private List<Button> _buttons = new List<Button>();
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
