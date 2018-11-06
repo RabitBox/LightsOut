643d2dfbd7b507a16284ffb5881fbc11b8bd91dd
Gai Takakura <g.takakura@outlook.com>
2018-11-06 20:50:17 +0900

    ScriptableObjectを使ってマテリアルの管理

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -2,33 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// ボタンに関連する処理をまとめたクラス
+/// </summary>
 public class Button : MonoBehaviour
 {
 	private bool _isLight = true;
 	public bool IsLight { get { return this._isLight; } }
+
+	[SerializeField]
 	MeshRenderer _renderer = null;
 
-	[SerializeField] GameData _data = null;
+	[SerializeField] private GameData _data = null;
 
-	// Use this for initialization
-	private void Start()
+	private bool _isClicked = false;
+	public bool IsClicked
 	{
-		_renderer = this.transform.GetChild(0).GetComponent<MeshRenderer>();
-		
+		get { return _isClicked; }
+		private set { _isClicked = value; }
 	}
 
+	// クリックされたときに外部で呼び出す
+	public void Click()
+	{
+		IsClicked = true;
+	}
+
+	/// <summary>
+	/// ボタンの発光を切り替える
+	/// </summary>
 	public void SwitchLight()
 	{
 		_isLight = (_isLight == true) ? 
 			false :
 			true;
+		_isClicked = false;
 
-		// 仮実装
 		SwitchMaterial();
 	}
 
-	// マテリアルを入れ替える
-	public void SwitchMaterial()
+	/// <summary>
+	/// マテリアルを入れ替える
+	/// </summary>
+	private void SwitchMaterial()
 	{
 		if (_renderer != null
 			&& _data != null)
---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// ボタンの配置や管理を行うクラス
+/// </summary>
+public class ButtonManager : MonoBehaviour
+{
+	[SerializeField] private Button[,] _buttons;
+	[SerializeField] private GameObject _buttonPrefab = null;
+
+	private readonly int NUMBER = 5;
+	private readonly float BUTTON_DISTANCE = 1.25f;
+
+	private int[,] _switchTable = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
+
+	//==================================================
+	// Use this for initialization
+	void Start()
+	{
+		CreateButtons();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Click();
+	}
+
+	/// <summary>
+	/// 画面をクリックされたときの処理
+	/// </summary>
+	private void Click()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			var target = InputManager.Instance.GetRaycastHitObject(Input.mousePosition);
+
+			if(target != null)
+			{
+				if(target.tag == "Button")
+				{
+					var button = target.GetComponent<Button>();
+					button.Click();
+					button.SwitchLight();	// 仮
+				}
+			}
+		}
+	}
+
+	/// <summary>
+	/// 全てのボタンの状態をチェックする
+	/// </summary>
+	/// <returns>光っているボタンがあるか否か</returns>
+	private bool CheckButtons()
+	{
+		var sequence =	from row	in Enumerable.Range(0, this._buttons.GetLength(0))
+						from column in Enumerable.Range(0, this._buttons.GetLength(1))
+						select new { row, column };
+
+		foreach(var index in sequence)
+		{
+			if(this._buttons[index.row, index.column].IsLight)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/// <summary>
+	/// ボタンの光を切り替える
+	/// </summary>
+	private void SwitchButtons()
+	{
+		foreach(var index in _switchTable)
+		{
+
+		}
+	}
+
+	/// <summary>
+	/// ボタンを生成する
+	/// </summary>
+	private void CreateButtons()
+	{
+		// ボタンを登録する配列の初期化
+		_buttons = new Button[NUMBER, NUMBER];
+
+		if (_buttonPrefab != null)
+		{
+			// ボタンを配置する起点となる座標
+			var basePosition = new Vector3(
+				((float)(this._buttons.GetLength(0) / 2) * -BUTTON_DISTANCE),
+				((float)(this._buttons.GetLength(1) / 2) * -BUTTON_DISTANCE));
+
+			var sequence =	from row	in Enumerable.Range(0, this._buttons.GetLength(0))
+							from column in Enumerable.Range(0, this._buttons.GetLength(1))
+							select new { row, column };
+
+			foreach (var index in sequence)
+			{
+				var createdObject = Instantiate(_buttonPrefab, this.transform);
+				createdObject.transform.position = (new Vector3(index.column, index.row) * BUTTON_DISTANCE) + basePosition;
+				this._buttons[index.row, index.column] = createdObject.GetComponent<Button>();
+			}
+		}
+	}
+}
